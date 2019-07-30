d1088e8d684e94ae41029ea1f7c2b72ad398221c
Hoshiyar Singh <41269862+hoshster@users.noreply.github.com>
2019-07-30 15:30:50 +0530

    Python code is attached...
    Not able to clear below error from the python run window (PyCharm):
    
    /usr/local/bin/python3.7 /Users/hoshiyar/PycharmProjects/FlowedFileReportExtraction.py/FlowedFileExtractorModule.py
    /Users/hoshiyar/PycharmProjects/FlowedFileReportExtraction.py
    /Library/Frameworks/Python.framework/Versions/3.7/lib/python3.7/site-packages/openpyxl/worksheet/_reader.py:296: UserWarning: Unknown extension is not supported and will be removed
      warn(msg)
    Traceback (most recent call last):
      File "/Users/hoshiyar/PycharmProjects/FlowedFileReportExtraction.py/FlowedFileExtractorModule.py", line 18, in <module>
        wb.active()
    TypeError: 'Worksheet' object is not callable

---
+++ FlowedFileExtractorModule.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python3
+
+import os
+import openpyxl
+
+print(os.getcwd())
+os.chdir(os.path.join(os.path.expanduser('~'), 'Desktop'))
+
+if not os.path.exists('FlowedFileReport'):
+    os.mkdir('FlowedFileReport')
+else:
+    pass
+
+os.chdir('FlowedFileReport')
+
+# open workbook
+wb = openpyxl.load_workbook(report.xlsx, data_only)
+wb.active()
+sh = wb['Sheet1']
+type(sh)
+
+
